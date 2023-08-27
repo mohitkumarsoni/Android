bd51fe04a67bb1727fa92d92d5aff3f6b48d76fe
Mohitkumar Soni <108524949+mohitkumarsoni@users.noreply.github.com>
2023-08-27 23:55:56 +0530

    Add files via upload

---
+++ collapsing toolbar layout/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.collapsingtoolbarlayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.palette.graphics.Palette;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbarLayout);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
